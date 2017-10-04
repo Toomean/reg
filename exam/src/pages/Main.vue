6fa04105bc78c3eac4afbf84e7234d5602695aa6
Tommy Toomean <arttumin@gmail.com>
2017-10-04 15:43:58 +0300

    implement simple grid

--- exam/src/pages/Main.vue
+++ exam/src/pages/Main.vue
@@ -1,7 +1,46 @@
 
 <template>
-	<div>
-		Main
+	<div class="Grid">
+		<div class="Grid__cell 4/12">
+			<div class="Grid__box">
+				4/12
+			</div>
+		</div>
+		<div class="Grid__cell 4/12">
+			<div class="Grid__box">
+				4/12
+			</div>
+		</div>
+		<div class="Grid__cell 4/12">
+			<div class="Grid__box">
+				4/12
+			</div>
+		</div>
+		<div class="Grid__cell 4/12">
+			<div class="Grid__box">
+				4/12
+			</div>
+		</div>
+		<div class="Grid__cell 4/12">
+			<div class="Grid__box">
+				4/12
+			</div>
+		</div>
+		<div class="Grid__cell 4/12">
+			<div class="Grid__box">
+				4/12
+			</div>
+		</div>
+		<div class="Grid__cell 8/12">
+			<div class="Grid__box">
+				8/12
+			</div>
+		</div>
+		<div class="Grid__cell 4/12">
+			<div class="Grid__box">
+				4/12
+			</div>
+		</div>
 	</div>
 </template>
 
