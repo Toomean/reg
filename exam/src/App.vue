c2074be0dd78153de38543be42ca55f176ebede6
Tommy Toomean <arttumin@gmail.com>
2017-10-04 13:59:26 +0300

    base carcass

--- exam/src/App.vue
+++ exam/src/App.vue
@@ -1,9 +1,34 @@
+
+<style lang="sass">
+
+</style>
+
 <template>
-<div class="app-layout">
-</div>
+    <div class="Wrapper">
+
+        <header class="Header">
+            <nav class="Nav">
+                <ul class="Nav__list">
+                    <li class="Nav__item">
+                        <a class="Nav__link" href="#">Home</a>
+                    </li>
+                    <li class="Nav__item">
+                        <a class="Nav__link" href="#">About</a>
+                    </li>
+                    <li class="Nav__item">
+                        <a class="Nav__link" href="#">Whatever</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <main>
+            Main
+        </main>
+
+    </div>
 </template>
+
 <script>
 export default {};
 </script>
-<style>
-</style>
