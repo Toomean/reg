4bc90fbf83df9500164b9ec01ffe6e0c5672171f
Tommy Toomean <arttumin@gmail.com>
2017-10-08 19:18:28 +0300

    add dynamic tiles

--- exam/src/pages/Main.vue
+++ exam/src/pages/Main.vue
@@ -1,49 +1,35 @@
 
+<style lang="sass">
+
+.Tile
+	
+	&__title
+
+	&__description
+
+</style>
+
 <template>
 	<div class="Grid">
-		<div class="Grid__cell 4/12">
+		<div class="Grid__cell" :class="[`Grid__cell_${tile.type}`]" v-for="tile in allTiles">
 			<div class="Grid__box">
-				<img style="width:100%" :src="'http://placekitten.com/g/300/300'">
-			</div>
-		</div>
-		<div class="Grid__cell 4/12">
-			<div class="Grid__box">
-				<img style="width:100%" :src="'http://placekitten.com/g/400/400'">
-			</div>
-		</div>
-		<div class="Grid__cell 4/12">
-			<div class="Grid__box">
-				<img style="width:100%" :src="'http://placekitten.com/g/500/500'">
-			</div>
-		</div>
-		<div class="Grid__cell 4/12">
-			<div class="Grid__box">
-				<img style="width:100%" :src="'http://placekitten.com/g/300/300'">
-			</div>
-		</div>
-		<div class="Grid__cell 4/12">
-			<div class="Grid__box">
-				<img style="width:100%" :src="'http://placekitten.com/g/400/400'">
-			</div>
-		</div>
-		<div class="Grid__cell 4/12">
-			<div class="Grid__box">
-				<img style="width:100%" :src="'http://placekitten.com/g/500/500'">
-			</div>
-		</div>
-		<div class="Grid__cell 8/12">
-			<div class="Grid__box">
-				<img style="width:100%" :src="'http://placekitten.com/g/300/300'">
-			</div>
-		</div>
-		<div class="Grid__cell 4/12">
-			<div class="Grid__box">
-				<img style="width:100%" :src="'http://placekitten.com/g/400/400'">
+				<a class="Tile" :style="{ backgroundImage: 'url(http://placekitten.com/g/300/300)' }">
+					<h2 class="Tile__title">{{ tile.title }}</h2>
+					<p class="Tile__desc">{{ tile.description }}</p>
+				</a>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-export default {}
+import { mapGetters } from 'vuex'
+
+export default {
+    created () {
+        this.$store.dispatch('getAllTiles')
+    },
+
+    computed: mapGetters([ 'allTiles' ])
+}
 </script>
