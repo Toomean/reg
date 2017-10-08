11ddf3d2a9ff288f3e85df09f4581d158372ee4c
Tommy Toomean <arttumin@gmail.com>
2017-10-08 17:55:52 +0300

    use vex

--- exam/src/index.js
+++ exam/src/index.js
@@ -1,10 +1,12 @@
-import Vue from 'vue';
+import Vue from 'vue'
 
-import App from './App.vue';
+import App from './App.vue'
 import router from './router'
+import store from './vuex/store'
 
 const app = new Vue({
-	router,
+    router,
+    store,
     render(h) {
         return h(App);
     },
