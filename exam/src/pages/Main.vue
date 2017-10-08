bba9239b36571c79adb7f6514bbf1706d46afebb
Tommy Toomean <arttumin@gmail.com>
2017-10-08 19:24:20 +0300

    tile base styling

--- exam/src/pages/Main.vue
+++ exam/src/pages/Main.vue
@@ -2,7 +2,29 @@
 <style lang="sass">
 
 .Tile
+	position: relative
+	display: block
+
+	min-height: 100%
+	padding: 1rem
+
+	color: white
+
+	&:before
+		content: ''
+
+		position: absolute
+		top: 0
+		left: 0
+
+		width: 100%
+		height: 100%
+
+		background: rgba(0, 0, 0, .5)
 	
+	&__wrapper
+		position: relative
+
 	&__title
 
 	&__description
@@ -14,8 +36,10 @@
 		<div class="Grid__cell" :class="[`Grid__cell_${tile.type}`]" v-for="tile in allTiles">
 			<div class="Grid__box">
 				<a class="Tile" :style="{ backgroundImage: 'url(http://placekitten.com/g/300/300)' }">
-					<h2 class="Tile__title">{{ tile.title }}</h2>
-					<p class="Tile__desc">{{ tile.description }}</p>
+					<div class="Tile__wrapper">
+						<h2 class="Tile__title">{{ tile.title }}</h2>
+						<p class="Tile__desc">{{ tile.description }}</p>
+					</div>
 				</a>
 			</div>
 		</div>
